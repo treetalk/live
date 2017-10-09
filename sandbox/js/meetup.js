8d615a7a9940b27db07e56396291500b0d7769aa
Austin C <ac6404@gmail.com>
2017-10-09 11:04:10 -0500

    minor format updates

--- sandbox/js/meetup.js
+++ sandbox/js/meetup.js
@@ -1,39 +1,34 @@
 // Meetup API request using jquery ajax method. Currently you will need to insert your own API key below in order to get this to work. The current callback function for a successful request simply prints the response object to the console.
 
-//
+
 // NOTES:
-//   -using meetup api v2 for open_events:
-//    https://www.meetup.com/meetup_api/docs/2/open_events/
-//    As of now it seems like newer api v3 does not
-//    support methods that filter requests by event category.
-//
-//   -need to find a way to disentangle community events and environment events from meetup's
-//    'community & environment' category. Also, it seems other relevant events exist in other //      categories and get left out...
+// using meetup api v2 for open_events: https://www.meetup.com/meetup_api/docs/2/open_events/ As of now it seems like newer api v3 does not support methods that filter requests by event category.
+
+//need to find a way to disentangle community events and environment events from meetup's 'community & environment' category. Also, it seems other relevant events exist in other categories and get left out...
 
 
-//************************************************************************************************
-//                               REQUEST OPTIONS                                              //
-//************************************************************************************************
-var category = '4';         // 4 is the meetup category for 'Community & Environment'
-var timespan = ',2w'        //current to 2wks from now. format is 'weeks before today, weeks after"
+//**************************
+//    REQUEST OPTIONS  //
+//**************************
+var category = '4';   // 4 is 'Community & Environment'
+var timespan = ',2w'  //format is 'weeks before today, weeks after"
 
-                            //coordinates currently set to St. Louis. Eventually should be able to pull user coords from mapping framework.
-var userLat = 38.643497;
+var userLat = 38.643497; //test coords set to STL
 var userLon = -90.28338;
 
 
-var radius = 'smart';       //search radius. 'Smart' option adjusts based on event density.
-var fields = 'group_photo'; //additional fields to return. seperate multiple options with commas
+var radius = 'smart';       //search radius. 'Smart' based on event density.
+var fields = 'group_photo'; //additional fields to return.
 
-//********************************************
-//      Insert your API Key Here            //
-//********************************************
-var APIkey = '**********************';
+//**********************************
+//    Insert your API Key Here   //
+//*********************************
+var APIkey = '*********';
 
 
-//**********************************************************************************************
-//                              REQUEST BODY                                              //
-//**********************************************************************************************
+//*******************
+//   REQUEST BODY  //
+//********************
 function loadMeetups(){
   $.ajax({
     type: 'GET',
